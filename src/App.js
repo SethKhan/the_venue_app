2d26fd70925af2e9bc0bfdefcaebdb01fb0ba56e
SethKhan <SethKhan27@gmail.com>
2022-01-14 19:55:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Element} from 'react-scroll';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueNfo from './components/venueNfo';
-import Highlight from './components/Highlights';
+import Highlights from './components/Highlights';
 import Pricing from './components/pricing';
 import Location from './components/location';
 import Footer from './components/header_footer/Footer';
@@ -25,7 +25,7 @@ class App extends Component {
         </Element>
 
         <Element name="highlights">
-          <Hightlight/>
+          <Highlights/>
         </Element>
 
         <Element name="pricing">
